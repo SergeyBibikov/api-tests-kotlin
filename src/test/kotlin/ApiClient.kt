c994a29a0d21e2520793924610c88b7ad1ff2615
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-12-14 20:47:54 +0400

    registration tests

--- src/test/kotlin/ApiClient.kt
+++ src/test/kotlin/ApiClient.kt
@@ -1,23 +1,20 @@
 package com.example.sergeybibikov.kotlin.api_tests
 
-import com.google.gson.annotations.SerializedName
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-
-interface Ready {
-    @GET("/ready")
-    fun ready(): Call<ReadyAnswer>
-}
-
-data class ReadyAnswer(@SerializedName("status") val status: String)
 
 class ApiClient {
     companion object {
-        fun ready(): Response<ReadyAnswer> {
+        fun ready(): Response<ReadyResponseBody> {
             return getBuiltClient(Ready::class.java).ready().execute()
+        }
+
+        fun register(username: String? = null, password: String?, email: String? )
+                : Response<RegisterResponseBody> {
+            return getBuiltClient(Register::class.java)
+                .register(RegisterRequestBody(username, password, email))
+                .execute()
         }
     }
 
--- src/test/kotlin/RegistrationTest.kt
+++ src/test/kotlin/RegistrationTest.kt
@@ -1,10 +1,31 @@
 package com.example.sergeybibikov.kotlin.api_tests
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.assertAll
+import kotlin.test.assertEquals
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RegistrationTest {
+    @BeforeAll
+    fun healthCheck(){
+        waitTillServiceIsUp(30)
+    }
+    //TODO: add random values for inputs
     @Test
     fun `Successful registration`(){
-
+        val response = ApiClient.register("Michael", "234Ff3453535", "test@gmail.com")
+        assertEquals(201, response.code())
+    }
+    @Test
+    fun `Expecting fail as the password has no uppercase letters`(){
+        val expectedMessage = "The password must contain uppercase and lowercase letters"
+        val response = ApiClient.register("Michael12", "234ff3453535", "1test@gmail.com")
+        val errorMessage = getResponseErrorMessage(response.errorBody()?.string())
+        assertAll(
+            { assertEquals(400, response.code()) },
+            { assertEquals(expectedMessage, errorMessage) },
+        )
     }
 }
