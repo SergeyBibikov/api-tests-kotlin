f68cf136c70ef4bca21457bd330fc49b842b3eed
SergeyBibikov <sergey.bibikov.92@mail.ru>
2022-12-15 21:27:14 +0400

    build stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ repositories { mavenCentral() }
 dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.postgresql:postgresql:42.5.1")
 
     testImplementation(kotlin("test"))
 }
