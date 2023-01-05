b9bdc821f313dfbfba0d1952cf1e80a5058ad63d
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-01-05 21:13:50 +0400

    teams by name tests

--- src/test/kotlin/TeamsTest.kt
+++ src/test/kotlin/TeamsTest.kt
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import org.junit.jupiter.params.provider.ValueSource
 
 @Feature("Getting teams list")
 @Test
@@ -33,6 +34,31 @@ class TeamsTest {
         assertAll(
             { assertThat(resp.code()).isEqualTo(200) },
             { assertThat(resp.body()?.size).isEqualTo(30) })
+    }
+
+    @TeamsPositiveTest
+    @Test
+    @DisplayName("Should get one team with name Utah Jazz")
+    fun getTeamByName() {
+        val teamName = "Utah Jazz"
+        val resp = ApiClient.getTeams(name = teamName)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(1) },
+            { assertThat(body?.get(0)?.name).isEqualTo(teamName) })
+    }
+
+    @DisplayName("Should no teams with name Utam Jazz")
+    @TeamsNegativeTest
+    fun noTeamsWithInvalidName() {
+        val teamName = "Utam Jazz"
+        val resp = ApiClient.getTeams(name = teamName)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(0) },
+        )
     }
 
     @TeamsPositiveTest
@@ -88,6 +114,66 @@ class TeamsTest {
         assertAll(
             { assertThat(resp.code()).isEqualTo(400) },
             { assertThat(err).isEqualTo(expectedErrorMsg) }
+        )
+    }
+
+    @TeamsPositiveTest
+    @Story("Getting teams est. in a specific year")
+    @DisplayName("Should get only one team est. in ")
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(ints = [2008, 1984, 1997, 2001, 1960])
+    fun yearsWhenOneTeamWasEstablished(year: Int) {
+        val resp = ApiClient.getTeams(estYear = year)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(1) },
+            { assertThat(body?.get(0)?.estYear).isEqualTo(year) }
+        )
+
+    }
+
+    @TeamsPositiveTest
+    @Story("Getting teams est. in a specific year")
+    @DisplayName("Should get two teams est. in ")
+    @ParameterizedTest(name = "{0}")
+    @ValueSource(ints = [1989, 1971, 1970, 1946, 1976])
+    fun yearsWhenTwoTeamsWereEstablished(year: Int) {
+        val resp = ApiClient.getTeams(estYear = year)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(2) },
+            { assertThat(body?.filter { it.estYear != year }).isEmpty() }
+        )
+
+    }
+
+    @Test
+    @TeamsPositiveTest
+    @Story("Getting teams est. in a specific year")
+    @DisplayName("Should get three teams est. in 1968")
+    fun yearWhenThreeTeamsWereEstablished() {
+        val yearWithThreeTeams = 1968
+        val resp = ApiClient.getTeams(estYear = yearWithThreeTeams)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(3) },
+            { assertThat(body?.filter { it.estYear != yearWithThreeTeams }).isEmpty() }
+        )
+    }
+
+    @TeamsNegativeTest
+    @Story("Getting teams est. in a specific year")
+    @DisplayName("Should get no teams est. in 2020")
+    fun yearWhenNOTeamsWereEstablished() {
+        val yearWithNoTeams = 2020
+        val resp = ApiClient.getTeams(estYear = yearWithNoTeams)
+        val body = resp.body()
+        assertAll(
+            { assertThat(resp.code()).isEqualTo(200) },
+            { assertThat(body?.size).isEqualTo(0) },
         )
     }
 }
