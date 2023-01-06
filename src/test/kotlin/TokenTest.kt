5245693215eba0ba7424238e23806fe964807d99
SergeyBibikov <sergey.bibikov.92@mail.ru>
2023-01-06 22:50:00 +0400

    successful get token test

--- src/test/kotlin/TokenTest.kt
+++ src/test/kotlin/TokenTest.kt
@@ -1,8 +1,7 @@
 package com.example.sergeybibikov.kotlin.api_tests
 
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.TestInstance
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.parallel.Execution
 import org.junit.jupiter.api.parallel.ExecutionMode
 
@@ -12,4 +11,19 @@ import org.junit.jupiter.api.parallel.ExecutionMode
 class TokenTest {
     @BeforeAll
     fun healthCheck() = waitTillServiceIsUp(30)
+
+    @Test
+    fun successfulGetTokenRequest() {
+        val dbClient = DBClient()
+        val user = dbClient.getRandomUser()
+        val expectedRole = dbClient.getRoleById(user.roleId)
+
+        val resp = ApiClient.getToken(user.username, user.password)
+        val (role, _, uName) = resp.body()?.token!!.split("_")
+        assertAll(
+            { checkResponseStatus(resp, 200) },
+            { assertThat(role).isEqualTo(expectedRole.name) },
+            { assertThat(uName).isEqualTo(user.username) }
+        )
+    }
 }
